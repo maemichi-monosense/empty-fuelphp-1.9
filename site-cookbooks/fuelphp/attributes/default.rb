320809f8581f40162b13f91b671e4465585f217e
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-13 13:55:02 +0900

    Refactor ref

--- site-cookbooks/fuelphp/attributes/default.rb
+++ site-cookbooks/fuelphp/attributes/default.rb
@@ -1,9 +1,9 @@
 default['fuelphp']['host_name'] = 'vhost'
 default['fuelphp']['FQDN'] = 'www.monosense.com'
 default['fuelphp']['www'] = '/var/www'
-default['fuelphp']['deploy'] = '/var/www/fuelphp'
-default['fuelphp']['current'] = '/var/www/fuelphp/current'
-default['fuelphp']['doc_root'] = '/var/www/fuelphp/current/public'
+default['fuelphp']['deploy'] = "#{default['fuelphp']['www']}/fuelphp"
+default['fuelphp']['current'] = "#{default['fuelphp']['deploy']}/current"
+default['fuelphp']['doc_root'] = "#{default['fuelphp']['current']}/public"
 default['fuelphp']['index'] = 'index.php'
 default['fuelphp']['group']['www'] = 'www'
 default['fuelphp']['timezone'] = 'Asia/Tokyo'
